ead4c14f9a476fa2b4b96960176df0666f3a8ea4
Jonathan Wong <jonathan.wong110@gmail.com>
2019-08-01 21:59:38 +0000

    Automatically backed up by Learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,6 +10,7 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(params[:figure])
     @figure.title = Title.create(params[:title])
+    @figure.landmarks << Landmark.create(params[:landmark])
     binding.pry
     redirect "/figures/#{@figure.id}"
   end
